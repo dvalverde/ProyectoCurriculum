77eb80b9ce50cb1df06d2e0f54a4e1ea35930693
Edwin Liang Tang <43522426+EdwinLT@users.noreply.github.com>
2021-09-23 08:03:04 -0600

    SCV-7 Mover codigo de redireccion a un middleware

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $installation_file = storage_path('installed');
-    if (! file_exists($installation_file)) {
-        return redirect('/install');
-    }
-
     return view('welcome');
 });
