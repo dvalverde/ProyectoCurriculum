c84b8c7a37cbe6bd1f1b529166a1465b6ca8e394
Edwin Liang Tang <43522426+EdwinLT@users.noreply.github.com>
2021-09-23 00:33:03 -0600

    SCV-7 Agregar redireccionamiento al instalador
    Hacer que la pagina principal retorne un redirect a /install
    si el sistema aún no está configurado.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $installation_file = storage_path('installed');
+    if (! file_exists($installation_file)) {
+        return redirect('/install');
+    }
+
     return view('welcome');
 });
