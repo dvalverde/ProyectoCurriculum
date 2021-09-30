69fef9ddc368fa895825f78f6a781cd3210edbe5
Edwin Liang Tang <43522426+EdwinLT@users.noreply.github.com>
2021-09-30 00:28:38 -0600

    SCV-12 Crear formulario habilidad

--- app/Http/Controllers/HabilidadController.php
+++ app/Http/Controllers/HabilidadController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Habilidad;
+use Illuminate\Support\Facades\Auth;
 
 class HabilidadController extends Controller
 {
-    
+    public function create()
+    {
+        return view('habilidad.create');
+    }
+
+    public function store()
+    {
+        Habilidad::create([
+            'id_usuario' => Auth::id(),
+            'descripcion' => request('descripcion'),
+            'dominio' => request('dominio'),
+        ]);
+    }
 }
---
+++ resources/views/habilidad/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <title>Registro de Habilidad</title>
+</head>
+<body>
+    <h1>Registrar habilidad</h1>
+    <form method="POST" action= {{ route('habilidad.store') }} >
+        @csrf
+        <label>
+            Descripción
+            <input type="text" name="descripcion">
+        </label>
+        <label>
+            Dominio
+            <input type="number" name="dominio" min="1" max="10">
+        </label>
+        <button>Guardar</button>
+    </form>
+</body>
