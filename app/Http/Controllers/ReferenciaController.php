38dd8f7094bb0e957835f3e6e098c8d16b9a6dd3
Edwin Liang Tang <43522426+EdwinLT@users.noreply.github.com>
2021-10-04 11:37:50 -0600

    SCV-13 Implementar formulario para crear referencias

--- app/Http/Controllers/ReferenciaController.php
+++ app/Http/Controllers/ReferenciaController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReferenciaController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('referencia.create');
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'email_contacto' => 'required|email',
+            'telefono_contacto' => 'required|numeric'
+        ]);
+
+        $user = Auth::user();
+        $user->referencias()->create(request()->all());
+
+        return redirect('/');
+    }
 }
